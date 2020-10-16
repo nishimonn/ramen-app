791f47069c6ad078b958dcf0b3f152a8a99e33bf
nishimonn <n.toppo.s.n@gmail.com>
2020-10-16 15:41:36 +0900

    ラーメンshopモデル作成

--- db/schema.rb
+++ db/schema.rb
@@ -10,7 +10,13 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 2020_10_16_045910) do
+ActiveRecord::Schema.define(version: 2020_10_16_054111) do
+
+  create_table "ramen_shops", options: "ENGINE=InnoDB DEFAULT CHARSET=utf8", force: :cascade do |t|
+    t.string "name"
+    t.datetime "created_at", precision: 6, null: false
+    t.datetime "updated_at", precision: 6, null: false
+  end
 
   create_table "users", options: "ENGINE=InnoDB DEFAULT CHARSET=utf8", force: :cascade do |t|
     t.string "email", default: "", null: false
--- db/seeds.rb
+++ db/seeds.rb
@@ -5,3 +5,7 @@
 #
 #   movies = Movie.create([{ name: 'Star Wars' }, { name: 'Lord of the Rings' }])
 #   Character.create(name: 'Luke', movie: movies.first)
+RamenShop.create(name: "むらた亭")
+RamenShop.create(name: "石田一龍")
+RamenShop.create(name: "達磨")
+RamenShop.create(name: "山小屋")
