a8f2ec728d0f807a267e9ea3ea4edb1966dfb35d
nishimonn <n.toppo.s.n@gmail.com>
2020-10-16 16:01:53 +0900

    ルーティング記述

--- config/routes.rb
+++ config/routes.rb
@@ -1,6 +1,7 @@
 Rails.application.routes.draw do
-  get 'reviews/index'
   devise_for :users
   root to: "homes#index"
-  resources :ramen_shops, only: [:index, :show]
+  resources :ramen_shops, only: [:index, :show] do
+    resources :reviews, only: [:index, :create]
+  end
 end
